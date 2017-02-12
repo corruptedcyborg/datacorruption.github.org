219add7c26061bd0db03af4be3b6b02f542db016
corruptedcyborg <erikbgood@gmail.com>
2017-02-12 22:14:50 +0100

    Awesome

---
+++ Login.php
@@ -0,0 +1,18 @@
+
+
+<?php
+    session_start();
+
+    if (isset($_SESSION[]))
+
+
+
+<html> 
+    <form id="password" action="login.php" method="POST">
+        <input type="password" name="pw" id="textfield" maxlength="21"><br />
+                <br />
+                    
+                <br />
+         <input type="image" src="enter_button.png"      onMouseOver="this.src='enter_button_hover.png'"    onMouseOut="this.src='enter_button.png'" alt="Enter">
+     </form>
+</html>
